d8d2c3c5c3d0b8339e562fa750b070286aae9dac
Jan Kousalík <kousaja1@gmail.com>
2025-01-27 12:52:44 +0100

    Main menu + empty gallery activity

---
+++ app/src/main/java/no/hvl/dat153/quizapp/activities/GalleryActivity.kt
@@ -0,0 +1,13 @@
+package no.hvl.dat153.quizapp.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import no.hvl.dat153.quizapp.R
+
+class GalleryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gallery)
+    }
+}
--- app/src/main/java/no/hvl/dat153/quizapp/activities/MainActivity.kt
+++ app/src/main/java/no/hvl/dat153/quizapp/activities/MainActivity.kt
@@ -1,10 +1,13 @@
-package no.hvl.dat153.quizapp
+package no.hvl.dat153.quizapp.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import no.hvl.dat153.quizapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +18,13 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val buttonGallery = findViewById<Button>(R.id.buttonGallery)
+        val buttonQuiz = findViewById<Button>(R.id.buttonQuiz)
+
+        buttonGallery.setOnClickListener {
+            startActivity(Intent(this, GalleryActivity::class.java))
         }
     }
 }
